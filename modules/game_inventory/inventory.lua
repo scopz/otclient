71f114c46bfca80399a4044270c011493244c9e6
scopp <osc.car2@gmail.com>
2020-04-11 16:08:39 +0200

    Joined combatcontrols and inventory

--- modules/game_inventory/inventory.lua
+++ modules/game_inventory/inventory.lua
@@ -16,21 +16,55 @@ inventoryPanel = nil
 inventoryButton = nil
 purseButton = nil
 
+fightOffensiveBox = nil
+fightBalancedBox = nil
+fightDefensiveBox = nil
+chaseModeButton = nil
+safeFightButton = nil
+whiteDoveBox = nil
+whiteHandBox = nil
+yellowHandBox = nil
+redFistBox = nil
+mountButton = nil
+pvpModesPanel = nil
+fightModeRadioGroup = nil
+pvpModeRadioGroup = nil
+
 function init()
-  connect(LocalPlayer, {
-    onInventoryChange = onInventoryChange,
-    onBlessingsChange = onBlessingsChange
-  })
-  connect(g_game, { onGameStart = refresh })
-
-  g_keyboard.bindKeyDown('Ctrl+I', toggle)
-
   inventoryButton = modules.client_topmenu.addRightGameToggleButton('inventoryButton', tr('Inventory') .. ' (Ctrl+I)', '/images/topbuttons/inventory', toggle)
   inventoryButton:setOn(true)
 
   inventoryWindow = g_ui.loadUI('inventory', modules.game_interface.getRightPanel())
   inventoryWindow:disableResize()
   inventoryPanel = inventoryWindow:getChildById('contentsPanel')
+
+  fightOffensiveBox = inventoryWindow:recursiveGetChildById('fightOffensiveBox')
+  fightBalancedBox = inventoryWindow:recursiveGetChildById('fightBalancedBox')
+  fightDefensiveBox = inventoryWindow:recursiveGetChildById('fightDefensiveBox')
+
+  chaseModeButton = inventoryWindow:recursiveGetChildById('chaseModeBox')
+  safeFightButton = inventoryWindow:recursiveGetChildById('safeFightBox')
+
+  mountButton = inventoryWindow:recursiveGetChildById('mountButton')
+  mountButton.onClick = onMountButtonClick
+
+  pvpModesPanel = inventoryWindow:recursiveGetChildById('pvpModesPanel')
+
+  whiteDoveBox = inventoryWindow:recursiveGetChildById('whiteDoveBox')
+  whiteHandBox = inventoryWindow:recursiveGetChildById('whiteHandBox')
+  yellowHandBox = inventoryWindow:recursiveGetChildById('yellowHandBox')
+  redFistBox = inventoryWindow:recursiveGetChildById('redFistBox')
+
+  fightModeRadioGroup = UIRadioGroup.create()
+  fightModeRadioGroup:addWidget(fightOffensiveBox)
+  fightModeRadioGroup:addWidget(fightBalancedBox)
+  fightModeRadioGroup:addWidget(fightDefensiveBox)
+
+  pvpModeRadioGroup = UIRadioGroup.create()
+  pvpModeRadioGroup:addWidget(whiteDoveBox)
+  pvpModeRadioGroup:addWidget(whiteHandBox)
+  pvpModeRadioGroup:addWidget(yellowHandBox)
+  pvpModeRadioGroup:addWidget(redFistBox)
 
   purseButton = inventoryPanel:getChildById('purseButton')
   local function purseFunction()
@@ -41,21 +75,62 @@ function init()
   end
   purseButton.onClick = purseFunction
 
-  refresh()
+
+  g_keyboard.bindKeyDown('Ctrl+I', toggle)
+
+  connect(fightModeRadioGroup, { onSelectionChange = onSetFightMode })
+  connect(pvpModeRadioGroup, { onSelectionChange = onSetPVPMode })
+  connect(chaseModeButton, { onCheckChange = onSetChaseMode })
+  connect(safeFightButton, { onCheckChange = onSetSafeFight })
+  connect(g_game, {
+    onGameStart = online,
+    onGameEnd = offline,
+    onFightModeChange = update,
+    onChaseModeChange = update,
+    onSafeFightChange = update,
+    onPVPModeChange   = update,
+    onWalk = check,
+    onAutoWalk = check
+  })
+
+  connect(LocalPlayer, {
+    onInventoryChange = onInventoryChange,
+    onBlessingsChange = onBlessingsChange,
+    onOutfitChange = onOutfitChange
+  })
+
+  online()
   inventoryWindow:setup()
 end
 
 function terminate()
-  disconnect(LocalPlayer, {
-    onInventoryChange = onInventoryChange,
-    onBlessingsChange = onBlessingsChange
-  })
-  disconnect(g_game, { onGameStart = refresh })
+  if g_game.isOnline() then
+    offline()
+  end
 
   g_keyboard.unbindKeyDown('Ctrl+I')
 
+  fightModeRadioGroup:destroy()
+  pvpModeRadioGroup:destroy()
   inventoryWindow:destroy()
   inventoryButton:destroy()
+
+  disconnect(g_game, {
+    onGameStart = online,
+    onGameEnd = offline,
+    onFightModeChange = update,
+    onChaseModeChange = update,
+    onSafeFightChange = update,
+    onPVPModeChange   = update,
+    onWalk = check,
+    onAutoWalk = check
+  })
+
+  disconnect(LocalPlayer, {
+    onInventoryChange = onInventoryChange,
+    onBlessingsChange = onBlessingsChange,
+    onOutfitChange = onOutfitChange
+  })
 end
 
 function toggleAdventurerStyle(hasBlessing)
@@ -67,8 +142,35 @@ function toggleAdventurerStyle(hasBlessing)
   end
 end
 
-function refresh()
+function update()
+  local fightMode = g_game.getFightMode()
+  if fightMode == FightOffensive then
+    fightModeRadioGroup:selectWidget(fightOffensiveBox)
+  elseif fightMode == FightBalanced then
+    fightModeRadioGroup:selectWidget(fightBalancedBox)
+  else
+    fightModeRadioGroup:selectWidget(fightDefensiveBox)
+  end
+
+  local chaseMode = g_game.getChaseMode()
+  chaseModeButton:setChecked(chaseMode == ChaseOpponent)
+
+  local safeFight = g_game.isSafeFight()
+  safeFightButton:setChecked(not safeFight)
+
+  if g_game.getFeature(GamePVPMode) then
+    local pvpMode = g_game.getPVPMode()
+    local pvpWidget = getPVPBoxByMode(pvpMode)
+    if pvpWidget then
+      pvpModeRadioGroup:selectWidget(pvpWidget)
+    end
+  end
+end
+
+function online()
   local player = g_game.getLocalPlayer()
+
+
   for i = InventorySlotFirst, InventorySlotPurse do
     if g_game.isOnline() then
       onInventoryChange(player, i, player:getInventoryItem(i))
@@ -77,8 +179,61 @@ function refresh()
     end
     toggleAdventurerStyle(player and Bit.hasBit(player:getBlessings(), Blessings.Adventurer) or false)
   end
+  
 
-  purseButton:setVisible(g_game.getFeature(GamePurseSlot))
+  if g_game.isOnline() then
+    local char = g_game.getCharacterName()
+
+    local lastCombatControls = g_settings.getNode('LastCombatControls')
+
+    if not table.empty(lastCombatControls) then
+      if lastCombatControls[char] then
+        g_game.setFightMode(lastCombatControls[char].fightMode)
+        g_game.setChaseMode(lastCombatControls[char].chaseMode)
+        g_game.setSafeFight(lastCombatControls[char].safeFight)
+        if lastCombatControls[char].pvpMode then
+          g_game.setPVPMode(lastCombatControls[char].pvpMode)
+        end
+      end
+    end
+
+    if g_game.getFeature(GamePlayerMounts) then
+      mountButton:setVisible(true)
+      mountButton:setChecked(player:isMounted())
+    else
+      mountButton:setVisible(false)
+    end
+
+    pvpModesPanel:setVisible(g_game.getFeature(GamePVPMode))
+    purseButton:setVisible(g_game.getFeature(GamePurseSlot))
+
+    update()
+  end
+
+end
+
+function offline()
+  local lastCombatControls = g_settings.getNode('LastCombatControls')
+  if not lastCombatControls then
+    lastCombatControls = {}
+  end
+
+  local player = g_game.getLocalPlayer()
+  if player then
+    local char = g_game.getCharacterName()
+    lastCombatControls[char] = {
+      fightMode = g_game.getFightMode(),
+      chaseMode = g_game.getChaseMode(),
+      safeFight = g_game.isSafeFight()
+    }
+
+    if g_game.getFeature(GamePVPMode) then
+      lastCombatControls[char].pvpMode = g_game.getPVPMode()
+    end
+
+    -- save last combat control settings
+    g_settings.setNode('LastCombatControls', lastCombatControls)
+  end
 end
 
 function toggle()
@@ -88,6 +243,14 @@ function toggle()
   else
     inventoryWindow:open()
     inventoryButton:setOn(true)
+  end
+end
+
+function check()
+  if modules.client_options.getOption('autoChaseOverride') then
+    if g_game.isAttacking() and g_game.getChaseMode() == ChaseOpponent then
+      g_game.setChaseMode(DontChase)
+    end
   end
 end
 
@@ -121,4 +284,67 @@ function onBlessingsChange(player, blessings, oldBlessings)
   if hasAdventurerBlessing ~= Bit.hasBit(oldBlessings, Blessings.Adventurer) then
     toggleAdventurerStyle(hasAdventurerBlessing)
   end
+end
+
+function onMountButtonClick(self, mousePos)
+  local player = g_game.getLocalPlayer()
+  if player then
+    player:toggleMount()
+  end
+end
+
+function onOutfitChange(localPlayer, outfit, oldOutfit)
+  if outfit.mount == oldOutfit.mount then
+    return
+  end
+
+  mountButton:setChecked(outfit.mount ~= nil and outfit.mount > 0)
+end
+
+function onSetFightMode(self, selectedFightButton)
+  if selectedFightButton == nil then return end
+  local buttonId = selectedFightButton:getId()
+  local fightMode
+  if buttonId == 'fightOffensiveBox' then
+    fightMode = FightOffensive
+  elseif buttonId == 'fightBalancedBox' then
+    fightMode = FightBalanced
+  else
+    fightMode = FightDefensive
+  end
+  g_game.setFightMode(fightMode)
+end
+
+function onSetChaseMode(self, checked)
+  local chaseMode
+  if checked then
+    chaseMode = ChaseOpponent
+  else
+    chaseMode = DontChase
+  end
+  g_game.setChaseMode(chaseMode)
+end
+
+function onSetSafeFight(self, checked)
+  g_game.setSafeFight(not checked)
+end
+
+function onSetPVPMode(self, selectedPVPButton)
+  if selectedPVPButton == nil then
+    return
+  end
+
+  local buttonId = selectedPVPButton:getId()
+  local pvpMode = PVPWhiteDove
+  if buttonId == 'whiteDoveBox' then
+    pvpMode = PVPWhiteDove
+  elseif buttonId == 'whiteHandBox' then
+    pvpMode = PVPWhiteHand
+  elseif buttonId == 'yellowHandBox' then
+    pvpMode = PVPYellowHand
+  elseif buttonId == 'redFistBox' then
+    pvpMode = PVPRedFist
+  end
+
+  g_game.setPVPMode(pvpMode)
 end

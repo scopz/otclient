79e4a44097cbb995b30f72d0287476d81fca9d67
scopp <osc.car2@gmail.com>
2021-08-08 17:33:29 +0200

    [UI] Otc::GameBlueNpcNameColor

--- src/client/game.cpp
+++ src/client/game.cpp
@@ -1547,6 +1547,7 @@ void Game::setClientVersion(int version)
         enableFeature(Otc::GameDoubleFreeCapacity);
         enableFeature(Otc::GamePlayerStateU16);
         enableFeature(Otc::GameClientPing);
+        enableFeature(Otc::GameBlueNpcNameColor);
     }
 
     if (version >= 780) {
